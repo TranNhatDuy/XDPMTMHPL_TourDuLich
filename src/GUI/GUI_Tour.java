a0528d812445c3740e3011757401153bc03dbab6
TranNhatDuy <nhatduytran113@gmail.com>
2021-02-02 20:46:38 +0700

    Update
    Hiện thị giao diện

--- src/GUI/GUI_Tour.java
+++ src/GUI/GUI_Tour.java
@@ -5,6 +5,14 @@
  */
 package GUI;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JButton;
+import GUI.Panel_CN1;
+//import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
+import java.awt.Color;
+
+
 /**
  *
  * @author Win 10
@@ -16,6 +24,11 @@ public class GUI_Tour extends javax.swing.JFrame {
      */
     public GUI_Tour() {
         initComponents();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = screenSize.width/2;
+        int x1 = x/2;
+        this.setBounds(x1, 100, 700, 500);
+        this.setResizable(false);
     }
 
     /**
@@ -27,25 +40,137 @@ public class GUI_Tour extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        btnCN1 = new javax.swing.JButton();
+        btnCN2 = new javax.swing.JButton();
+        btnCN3 = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(700, 500));
+
+        btnCN1.setText("Chức năng 1");
+        btnCN1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCN1ActionPerformed(evt);
+            }
+        });
+
+        btnCN2.setText("Chức năng 2");
+        btnCN2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCN2ActionPerformed(evt);
+            }
+        });
+
+        btnCN3.setText("Chức năng 3");
+        btnCN3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCN3ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnCN2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
+                    .addComponent(btnCN1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnCN3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(101, 101, 101)
+                .addComponent(btnCN1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnCN2, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnCN3, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(115, Short.MAX_VALUE))
+        );
+
+        jPanel3.setBackground(new java.awt.Color(102, 255, 102));
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 546, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+//    public static Panel_CN1 pn_cn1;
+//    public static Panel_CN2 pn_cn2;
+//    public static Panel_CN3 pn_cn3;
+    private void btnCN1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCN1ActionPerformed
+        // TODO add your handling code here:
+        if(evt.getSource() == btnCN1){
+            jPanel3.removeAll();
+            jPanel3.setBackground(Color.red);
+            Panel_CN1 pn_cn1 = new Panel_CN1();
+            jPanel3.add(pn_cn1);
+        }
+    }//GEN-LAST:event_btnCN1ActionPerformed
+
+    private void btnCN2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCN2ActionPerformed
+        // TODO add your handling code here:
+        
+        if(evt.getSource() == btnCN2){
+            jPanel3.removeAll();
+            jPanel3.setBackground(Color.yellow);
+            Panel_CN2 pn_cn2 = new Panel_CN2();
+            jPanel3.add(pn_cn2);
+        }
+    }//GEN-LAST:event_btnCN2ActionPerformed
+
+    private void btnCN3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCN3ActionPerformed
+        // TODO add your handling code here:
+        if(evt.getSource() == btnCN3){
+            jPanel3.removeAll();
+            jPanel3.setBackground(Color.black);
+            Panel_CN3 pn_cn3 = new Panel_CN3();
+            jPanel3.add(pn_cn3);
+        }
+    }//GEN-LAST:event_btnCN3ActionPerformed
+ 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -74,10 +199,19 @@ public class GUI_Tour extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new GUI_Tour().setVisible(true);
+//                pn_cn1.setVisible(false);
+//                pn_cn2.setVisible(false);
+//                pn_cn3.setVisible(false);
+                
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCN1;
+    private javax.swing.JButton btnCN2;
+    private javax.swing.JButton btnCN3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel3;
     // End of variables declaration//GEN-END:variables
 }
