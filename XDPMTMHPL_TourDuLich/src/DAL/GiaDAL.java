c511097d7449818c50e58286aff29d14534d1dac
voduykiet <voduykiet18@gmail.com>
2021-03-11 13:05:10 +0700

    up

--- XDPMTMHPL_TourDuLich/src/DAL/GiaDAL.java
+++ XDPMTMHPL_TourDuLich/src/DAL/GiaDAL.java
@@ -5,6 +5,9 @@
  */
 package DAL;
 
+import DTO.GiaDTO;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 /**
@@ -12,5 +15,28 @@ import java.util.ArrayList;
  * @author Vo Duy Kiet
  */
 public class GiaDAL {
-    public ArrayList<>
+
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+
+    public ArrayList<GiaDTO> loadDataGia() {
+        ArrayList<GiaDTO> gia = new ArrayList<>();
+        String query = "SELECT * FROM tour_gia";
+        try {
+            ps=new MySQLConnect().conn.prepareStatement(query);
+            rs=ps.executeQuery();
+            while(rs.next()){
+                GiaDTO g=new GiaDTO();
+                g.setMagia(rs.getString("gia_id"));
+                g.setSotien(rs.getInt("gia_sotien"));
+                g.setNgaydi(rs.getString("gia_tungay"));
+                g.setNgayden(rs.getString("gia_denngay"));
+                g.setMatour(rs.getString("tour_id"));
+                gia.add(g);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return gia;
+    }
 }
